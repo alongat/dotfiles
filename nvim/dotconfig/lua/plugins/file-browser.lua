75e5af2a22fd1f4b686c09382b620220f475b884
Alon Gat <alon@torq.io>
2024-05-09 12:55:33 +0300

    updates

--- nvim/dotconfig/lua/plugins/file-browser.lua
+++ nvim/dotconfig/lua/plugins/file-browser.lua
@@ -8,7 +8,17 @@ return {
     },
   },
   config = function()
-    require("telescope").load_extension("file_browser")
+    require("telescope").load_extension("file_browser").setup({
+      defaults = {
+        file_ignore_patterns = {},
+      },
+      extensions = {
+        file_browser = {
+          hidden = true,
+          show_hidden = true,
+        },
+      },
+    })
   end,
   dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
 }
