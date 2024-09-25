94263507486b3af1535462ea64d4359c0eaaa5ac
Alon Gat <alon@torq.io>
2024-09-25 22:33:25 +0300

    small

--- nvim/aliases.zsh
+++ nvim/aliases.zsh
@@ -1,3 +1,4 @@
 alias v="nvim ."
 alias vi='nvim'
 alias vim='nvim'
+alias -g W='nvim -c "setlocal buftype=nofile bufhidden=wipe" -c "nnoremap <buffer> q :q!<CR>" -'
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -4,4 +4,5 @@ alias ...="cd ../.."
 alias ls="eza --color=always --git --no-user --no-permissions --icons=always --group-directories-first"
 alias ll="ls --long"
 alias vd="viddy"
+alias sf='fzf -m --preview="bat --color=always {}" --bind "ctrl-w:execute(nvim {+})+abort,ctrl-y:execute-silent(echo {} | pbcopy)+abort"'
 alias c="clear"
