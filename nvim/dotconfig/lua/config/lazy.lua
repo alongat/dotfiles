4abb54cedf333574ce0182d93fa29e516404851f
Alon Gat <along@torq.io>
2024-12-19 20:42:07 +0200

    add avante

--- nvim/dotconfig/lua/config/lazy.lua
+++ nvim/dotconfig/lua/config/lazy.lua
@@ -21,7 +21,7 @@ require("lazy").setup({
     -- import/override with your plugins
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.util.project" },
-    { import = "lazyvim.plugins.extras.coding.copilot" },
+    -- { import = "lazyvim.plugins.extras.coding.copilot" },
     { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.ui.mini-animate" },
